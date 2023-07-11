6ed0984f98cbebe99e562fc4231fe2783d94cce4
Elias <e.borges289@gmail.com>
2023-07-11 12:18:06 -0300

    fix: cors

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,9 @@
 export class Api {
     static async getUserCity () {
-        const data = await fetch("https://api.hgbrasil.com/weather?key=83cd77f0&user_ip=remote", {
+        const data = await fetch("https://api.hgbrasil.com/weather?format=json-cors&key=83cd77f0&user_ip=remote", {
             method: "GET",
+            mode: "cors",
+            
         })
         .then(res => res.json())
         .then(res => {
@@ -11,8 +13,9 @@ export class Api {
         return data
     }
     static async getCityByName (name) {
-        const data = await fetch(`https://api.hgbrasil.com/weather?key=83cd77f0&city_name=${name}`, {
+        const data = await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=83cd77f0&city_name=${name}`, {
             method: "GET",
+            mode: "cors",
         })
         .then(res => res.json())
         .then(res => {
